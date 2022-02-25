ddbb34e80e704fe1c8fe3fa0556301bff378ceaf
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2022-02-25 17:44:53 +0530

    Add files via upload

---
+++ Task_2/Flag.js
@@ -0,0 +1,14 @@
+let request = new XMLHttpRequest();
+
+request.open("GET", "https://restcountries.com/v2/all");
+
+request.send()
+request.onload = function(){
+    
+    let flg = JSON.parse(request.responseText);
+    flg.map((i) => console.log(i.flags.svg))    
+};
+
+request.onerror = function (){
+    console.error("error");
+};
